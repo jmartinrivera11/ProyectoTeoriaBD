ebfe50336b3e4db25f1bd15c0d6fb87b545bb1e2
jmartinrivera11 <josemartinriverasolis@gmail.com>
2025-11-03 13:09:53 -0600

    Add Qt SQL support and database connection example
    Updated CMakeLists.txt to include Qt SQL components and link against Qt::Sql. Modified main.cpp to demonstrate connecting to a Firebird database using QSqlDatabase, replacing the previous MainWindow instantiation with a database connection test.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
-#include "mainwindow.h"
-
+// #include "mainwindow.h"
 #include <QApplication>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QCoreApplication a(argc, argv);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QIBASE");
+    db.setDatabaseName("PRESUPUESTO.fdb");
+    db.setUserName("sysdba");
+    db.setPassword("masterkey");
+
+    if (!db.open()) {
+        qDebug() << "Error:" << db.lastError().text();
+    } else {
+        qDebug() << "Conexión exitosa.";
+    }
+
     return a.exec();
 }
